8eb3cd9061f34de18e4354ee00f9ddcd88274989
Nicholas Bazzoni <nico@Nicholass-MBP.fios-router.home>
2023-10-21 10:00:22 -0400

    dalle erase

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import UfoSearch from './UfoSearch'
-import DALLE from './Dalle'
+
 import ufo from './assets/ufo.jpg'
 
 function App() {
